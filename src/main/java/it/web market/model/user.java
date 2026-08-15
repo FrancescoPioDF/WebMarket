8b1ef3f8d8ab259682114eabcbc03397a91794ee
Pio <Pio@DESKTOP-OLSPC1P.lan>
2026-08-15 09:49:54 +0200

    test collegamento git

---
+++ src/main/java/it/web market/model/user.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Pio
+ */
+
+public class user {
+    private Long userID;
+    
+    private String username;
+    
+    private String password;
+    
+    private String email;
+}
+
+private Long getuser(){
+    return userID;
+}
+
+private void setuserid(){
+
+}
+
+test git commit
